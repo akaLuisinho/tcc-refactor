02c34ff0173f668e303384c318734c441ed01aff
Luis Felipe <luis.felipeszsv@gmail.com>
2021-04-27 16:00:49 -0300

    adding secutiry to the login page

---
+++ src/model/Auth.js
@@ -0,0 +1,64 @@
+const Database = require('../db/config')
+
+module.exports = {
+    async get() {
+        const db = await Database()
+
+        const data = await db.all(`SELECT * FROM login`)
+
+        db.close()
+
+        return data
+    },
+    //async create(data) {
+    //    db = await Database()
+//
+    //    await db.run(`INSERT INTO appointments(
+    //        name,
+    //        address,
+    //        phone,
+    //        gender, 
+    //        birth_date,
+    //        dentist,
+    //        day,
+    //        time, 
+    //        email
+    //    ) VALUES(
+    //        "${data.name}",
+    //        "${data.address}",
+    //        "${data.phone}",
+    //        "${data.gender}",
+    //        "${data.birth_date}",
+    //        "${data.dentist}",
+    //        "${data.day}",
+    //        "${data.time}",
+    //        "${data.email}"
+    //    )` )
+//
+    //    await db.close()
+    //},
+    //async update(newData, id) {
+    //    db = await Database()
+//
+    //    await db.run(`UPDATE appointments SET
+    //        name = "${newData.name}",
+    //        address = "${newData.address}",
+    //        phone = "${newData.phone}",
+    //        gender = "${newData.gender}",
+    //        birth_date = "${newData.birth_date}",
+    //        dentist = "${newData.dentist}",
+    //        day = "${newData.day}",
+    //        time = "${newData.time}",
+    //        email = "${newData.email}"
+    //    WHERE id = ${id}` )
+//
+    //    await db.close()
+    //},
+    //async delete(id) {
+    //    db = await Database()
+//
+    //    await db.run(`DELETE FROM appointments WHERE id = ${id}`)
+    //    
+    //    await db.close()
+    //}
+}
